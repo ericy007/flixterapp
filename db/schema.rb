4267a24864fe3457abc51941ac74619edac91de8
Eric Yabit <eric.yabit@live.com>
2019-09-17 02:34:04 +0000

    Video uploading

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_001727) do
+ActiveRecord::Schema.define(version: 2019_09_17_021700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_09_17_001727) do
     t.string "title"
     t.string "subtitle"
     t.string "section_id"
+    t.string "video"
   end
 
   create_table "sections", id: :serial, force: :cascade do |t|
