69ca272e470334a3553ed2fd32547e8ba0e0ea96
Eric Yabit <eric.yabit@live.com>
2019-09-17 07:28:53 +0000

    removed addcolumn2users database

--- Gemfile
+++ Gemfile
@@ -76,3 +76,7 @@ gem "figaro"
 gem 'fog-aws'
 gem "mini_magick"
 gem 'carrierwave-aws'
+gem 'stripe'
+gem 'ranked-model'
+gem 'jquery-ui-rails'
+gem "font-awesome-rails"
--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -2,7 +2,6 @@ class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:show]
 
-
   def new
     @course = Course.new
   end
@@ -16,15 +15,16 @@ class Instructor::CoursesController < ApplicationController
     end
   end
 
-  def show
-    
+  def show 
+    @section = Section.new
+    @lesson = Lesson.new
   end
 
   private
 
   def require_authorized_for_current_course
     if current_course.user != current_user
-      render plain: "Unauthorized", status: :unauthorized
+      render plain: "Unauthorized", status: :unauthorized 
     end
   end
 
@@ -32,7 +32,6 @@ class Instructor::CoursesController < ApplicationController
   def current_course
     @current_course ||= Course.find(params[:id])
   end
-
 
   def course_params
     params.require(:course).permit(:title, :description, :cost, :image)
