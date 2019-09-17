3cda9f9067974e415e3ffebfb539247e2809f3f7
Your Name <you@example.com>
2019-09-17 07:28:53 +0000

    removed addcolumn2users database

--- db/migrate/20190916214837_update_users_column.rb
+++
@@ -1,8 +0,0 @@
-class UpdateUsersColumn < ActiveRecord::Migration[5.2]
-  def change
-    add_column  :lessons, :title, :string
-    add_column  :lessons, :subtitle, :string
-    add_column  :lessons, :section_id, :string
-    
-  end
-end
